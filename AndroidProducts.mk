10068f9250b894de9ada4336c0c7ecc80c852dbb
FIRxp011 <113378946+Firxp011@users.noreply.github.com>
2025-05-13 20:57:05 +0000

    update repo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,11 @@
-# Replace $$DEVICE$$ with your Device Name's Value.
-# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
-# The part of last line in mine looks like "omni_Primo_RX5.mk"
-
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_PATH)/twrp_a02.mk
+    $(LOCAL_DIR)/twrp_a02.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_a02-user \
-    twrp_a02-userdebug \
-	twrp_a02-eng
+COMMON_LUNCH_CHOICES := twrp_a02-eng
