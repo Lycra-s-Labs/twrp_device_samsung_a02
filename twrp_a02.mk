24112397917e078fe1bea7ac15a162f166cf8797
FIRxp011 <113378946+Firxp011@users.noreply.github.com>
2025-05-13 16:31:27 +0000

    update repo

--- twrp_a02.mk
+++ twrp_a02.mk
@@ -1,13 +1,18 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Another common config inclusion
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
-
-# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a02 device
+$(call inherit-product, device/samsung/a02/device.mk)
 
 
 # Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
